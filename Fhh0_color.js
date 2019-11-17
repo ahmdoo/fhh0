f33adc7a688997e00462382523d1efd09a88d186
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2019-11-17 22:05:00 +0300

    Create Fhh0_color.js

---
+++ Fhh0_color.js
@@ -0,0 +1,82 @@
+
+$(`<button id="clr1" class="colorsB" type="button" onclick="alwanAhmed();"></button>`).insertBefore('.nav-tabs');
+function alwanAhmed() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#fe6f27 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#2f2d29 !important';
+	}
+};
+
+$(`<button id="clr2" class="colorsB" type="button" onclick="alwanMuhannad1();"></button>`).insertBefore('.nav-tabs');
+function alwanMuhannad1() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#df678c !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#3d155f !important';
+	}
+};
+
+$(`<button id="clr3" class="colorsB" type="button" onclick="alwanAhmed2();"></button>`).insertBefore('.nav-tabs');
+function alwanAhmed2() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ elements[i].style.cssText = 'background-color:#185E40 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#89B0A3 !important';
+	}
+};
+
+$(`<button id="clr4" class="colorsB" type="button" onclick="alwanMuhannad2();"></button>`).insertBefore('.nav-tabs');
+function alwanMuhannad2() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ elements[i].style.cssText = 'background-color:#3b394a  !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#a6a4b5 !important';
+	}
+};
+$(`<button id="clr5" class="colorsB" type="button" onclick="alwanFahawa3anbr();"></button>`).insertBefore('.nav-tabs');
+function alwanFahawa3anbr() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#5c0e2f !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#fea780 !important';
+	}
+};
+$(`<button id="clr6" class="colorsB" type="button" onclick="alwanMai();"></button>`).insertBefore('.nav-tabs');
+function alwanMai() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#1e8ac0 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#f3c29d !important';
+	}
+};
+$(`<button id="clr7" class="colorsB" type="button" onclick="alwanFhh0();"></button>`).insertBefore('.nav-tabs');
+function alwanFhh0() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#ed3561!important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#761238!important';
+	}
+};
+$(`<button id="clr8" class="colorsB" type="button" onclick="alwan3anbr();"></button>`).insertBefore('.nav-tabs');
+function alwan3anbr() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#32ACBF !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#a6468f !important';
+	}
+};
