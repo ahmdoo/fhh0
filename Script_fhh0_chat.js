7d5af94fb515277e0bed7a8aff6eaf0fd52f4996
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2019-11-22 17:57:18 +0300

    Create Script_fhh0_chat.js

---
+++ Script_fhh0_chat.js
@@ -0,0 +1,341 @@
+$(`<style>a.label.label-primary.fl
+	{
+	display:none
+}
+button.btn.fr.btn-success.fa.fa-refresh
+	{
+	display:none
+}
+a.label.label-primary.fl img.fl
+
+a.label.label-primary.fl
+	{
+	border-radius:3px!important;
+	margin:2px-0px;
+	padding:5px!important;
+	margin-bottom:2px;
+	text-align:right!important
+}
+#des22
+	{
+	text-align:center!important;
+	font-size:100%!important
+}
+label.fl.label.loginstat
+	{
+
+	position:absolute;
+	top:0;
+	margin-top:4px;
+	Right:0
+}
+.fr.borderg,.fr.borderg a
+	{
+	font-size:10px!important;
+	padding:0 5px!important
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success
+	{
+	padding:0px 3px 2px;
+	margin-right:5px
+}
+
+	
+.fr.borderg,.fr.borderg a
+	{
+	border-radius:3px!important;
+	margin-right:0px!important;
+	border:black solid 0px;
+	color:#7D7D7D
+}
+ul.nav.nav-tabs>li>a
+	{
+	color:#7D7D7D;
+	border:none;
+	text-align:center;
+	border-radius:3px;
+	display:initial;
+	width:80%;
+	float:left;
+	padding:0!important
+}
+ul.nav.nav-tabs>li.active>a
+	{
+	border:none;
+	border-radius:3px
+}
+ul.nav.nav-tabs>li
+	{
+	width:32%;
+	margin:0 5px 0 0
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before
+	{
+	float:right;
+	margin-right:-26%;
+	border:1px solid;
+	padding:0 2px 1px;
+	border-radius:3px;
+	width:23px;
+	text-align:center;
+	color:#7D7D7D
+}
+ul.nav.nav-tabs
+	{
+	border-top:1px solid#190604;
+	margin-top:1px;
+	float:right;
+	width:100%;
+	padding:3px 1px
+}
+input#u1,input#u2,input#pass1,input#u3,input#pass2
+	{
+	width:50%;
+	height:31px;
+	margin-left:40.5px;
+	margin-bottom:5.5px;
+	border:1px solid#D1351D;
+	border-radius:3px;
+	text-align:center
+}
+div#asim13
+	{
+	width:49%;
+	top:122px;
+	position:absolute;
+	left:3px;
+
+}
+div#l1
+	{
+	padding:16.8px!important
+}
+}
+</style>`).insertBefore("body");
+
+$(`<div style="width:auto%;"  id="ahmed" id="sa" class="fa fa-paint-brush btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">اختر لونك</div>
+<div class="collapse" id="collapseExample"><center><div class="well"><p lass="bg-primary">  </p>إختر لونك بالضغط على احد الازرار  </p> <i class="fas fa-feather-alt"></i>
+ <button id="clr1" class="colorsB" type="button" onclick="alwanAhmed();
+function alwanAhmed() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#DFBBB1 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#2f2d29 !important';
+	}
+};"></button>
+
+<button id="clr2" class="colorsB" type="button" onclick="alwanMuhannad1();
+function alwanMuhannad1() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#ADA2B8 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#3d155f !important';
+	}
+};"></button>
+
+<button id="clr3" class="colorsB" type="button" onclick="alwanAhmed2();
+function alwanAhmed2() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ elements[i].style.cssText = 'background-color:#8AA2A9 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#717C89 !important';
+	}
+};"></button>
+
+<button id="clr4" class="colorsB" type="button" onclick="alwanMuhannad2();
+function alwanMuhannad2() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ elements[i].style.cssText = 'background-color:#3b394a  !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#a6a4b5 !important';
+	}
+};"></button>
+<button id="clr5" class="colorsB" type="button" onclick="alwanFahawa3anbr();
+function alwanFahawa3anbr() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#2E4756 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#9FA2B2 !important';
+	}
+};"></button>
+<button id="clr6" class="colorsB" type="button" onclick="alwanMai();
+function alwanMai() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#808F85 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#898579 !important';
+	}
+};"></button>
+<button id="clr7" class="colorsB" type="button" onclick="alwanFhh0();
+
+function alwanFhh0() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#A28F9D!important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#8B687F!important';
+	}
+};"></button>
+<button id="clr8" class="colorsB" type="button" onclick="alwan3anbr();
+function alwan3anbr() {	
+var elements = document.getElementsByClassName('label-primary'); 
+var btns = document.getElementsByClassName('btn-primary');
+for(var i = 0; i < elements.length; i++){ 
+elements[i].style.cssText = 'background-color:#538083 !important; width:100%;';
+}
+for(var i = 0; i < btns.length; i++){ btns[i].style.cssText = 'background-color:#52796F !important';
+	}
+};"></button>
+‏  <center></div></div></div>`).insertBefore('.nav-tabs');
+$(`<link rel="stylesheet" type="text/css" href="=-light,">
+<style>
+.ae {
+  border:1px solid #ffffff;
+}
+label.fl.label.loginstat
+	{
+
+	position:absolute;
+	top:0;
+	margin-top:4px;
+	Right:0
+}
+.nav-tabs {background-color: #ffffff!important;}
+ul.nav.nav-tabs.fl li a {
+  background:linear-gradient(163deg,#ffffff 5%,#ffffff 70%) !important;color: #434C4D;
+    margin: 0px 0px;
+    text-align: center!important;
+    width: 500px!important;
+  height: 300px!important;
+    border-radius: 50px!important;
+    height: 50% !important;
+    margin-top: 30px;
+    padding: 0px 0 !important;
+    font-family: 'jazeera',FontAwesome;
+    margin-right: 0px;
+    border: .60px solid !important;
+    float: right;
+}
+
+
+ul.nav.nav-tabs.fl li a:hover {
+  Background-color:#434C4D;width:100%;
+}
+
+.nav-tabs>li>a {
+  border:0 solid transparent;width:100%;
+}
+
+div#l1.grow.break.tab-pane.fade.active.in {
+    position: absolute !important;
+    width: 62% !important;
+    margin-top: 250px !important;
+    margin-left: 154px !important;
+    height: 97px;
+    background-color: #D9D9D9;
+    background-image: -webkit-linear-gradient(50deg,rgba(255,255,255,.2) 40%,transparent 40%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent) !important;
+    border-radius: 7px;
+    top: 220px;
+}
+
+div#l2,
+div#l3 {
+  position:absolute !important;
+  width: 62% !important;
+  margin-top: 147px !important;
+  margin-left: 150px !important;
+  height: 112px;
+  border-radius:7px;
+  background-color: #D9D9D9;
+  background-image:-webkit-linear-gradient(45deg,rgba(255,255,255,.2) 75%,transparent 75%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent) !important;
+  top: 310px;
+}
+
+
+div#l2 button.btn.btn-primary,
+div#l1 button.btn.btn-primary,
+div#l3 button.btn.btn-primary {
+  background: linear-gradient(163deg,#F0F0F0 0%,#000000 100%);
+}
+
+body.bg.center-block.bg.dad div#tlogins {
+  height:auto !important;
+}
+
+
+
+
+#team21 { transform:rotate(-25deg);
+    text-decoration: dotted;
+    color: #fff;
+    margin-top: 1.7px !important;
+    margin-left: 0px !important;
+    display: inline-block;
+    float: right;
+    margin-right: 1.5px !important;
+    box-shadow: 0 2px 2px 0 rgba(255,255,255,.2),0 1px 5px 0 rgba(255,255,255,.2),0 -3px 1px -2px rgba(255,255,255,.2) !important;
+    border-radius: 7px 14px 7px 14px !important;
+    border: #000000;
+}
+#team21 {
+    width: 55px;
+    padding: 2px 0px;
+    height: 19px;
+}
+
+
+div#l1 input#u1 {
+  width:105%;
+  margin-top:-3px;
+  margin-left:-5px !important;
+  text-align:center;
+  border-radius:18px;
+  height:30px;
+  border-color:#000000;
+}
+
+div#l2 input#u2,
+div#l3 input#u3 {
+  width:105%;
+  padding:0 !important;
+  margin-left:-4px !important;
+  text-align:center;
+  height:30px;
+  border-radius:18px;
+  margin-top:-4px !important;
+  border-color:#dbbcbc;
+}
+
+div#l2 input#pass1,
+div#l3 input#pass2 {
+  height:30px;
+  width:105%;
+  padding:0 !important;
+  border-radius:18px;
+  margin-top:2px;
+  margin-left:-4px !important;
+  text-align:center;
+  border-color:#F0F0F0;
+}
+
+
+
+.lonline.light.break {
+  outline:#000000 solid 3px !important;height: 161.609px;
+  background-color: #F2F2F2 !important;
+  background-image:-webkit-linear-gradient(50deg,rgba(255,255,255,.2) 25%,transparent 25%,transparent 50%,rgba(255,255,255,.2) 50%,rgba(255,255,255,.2) 75%,transparent 75%,transparent) !important;
+}
+
+
+</style>`).insertBefore('body');
