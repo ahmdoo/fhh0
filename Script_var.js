9c0133077b94e11b786b21c1a71db4c5954114b7
a7mdoof <57865371+a7mdoof@users.noreply.github.com>
2019-12-04 21:44:44 +0300

    Create Script_var.js

---
+++ Script_var.js
@@ -0,0 +1,98 @@
+$('.btn-primary').css({"background-image":"url(sico/1575471979567.jpg)"});
+$('.label-primary').css({"background-image":"url(sico/1575471979567.jpg)"});
+
+
+$('.nav-tabs').addClass('fl').css('width','100%');
+$("a.fa.fa-user").text('     دخـول ');
+$("a.fa.fa-user-plus").text('     تسجيـل عضويـة ');
+$('.loginstat').css({"background-image":"url(sico/1575471979567.jpg)"});
+
+$(`<div style="margin-top: -50px;width: 100%;background-image: url(sico/1566151816025.jpg);background-size: 100% auto;background-repeat: no-repeat;" class="fr">`).insertBefore('body');
+var Wally = function(){
+  $(".btn.minix.fa.fa-thumbs-up.fr").css("margin-right",45);
+$(".btn.minix.fa.fa-comment.fr").css("margin-Right",90);
+$(".btn.minix.fa.fa-times.fr").css("margin",2);
+};
+setInterval(Wally, 8000);
+$(`<center><div style="!important; "  id="ye" margin: 2px;" class="fa fa-heartbeat label label-primary" type="button" data-toggle="collapse" data-target="#demo" aria-expanded="false" aria-controls="collapseExample">&nbsp; ● ·( إضغـط وإستمـع  )· ● &nbsp;</div><div class="collapse" id="demo"><center><div class="well"><p lass="bg-primary"></i></p>  🎶 كل يوم اغنيـه من ذوقك </p><iframe width="100%" height="600" scrolling="no" frameborder="no" allow="autoplay" src="https://m.soundcloud.com"></iframe>`).insertBefore('#d2');
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="sico/1566151816025.jpg" class="fl" style="margin-right: 2px; width: 25px;">');
+$(``).insertBefore('div#chats');
+
+$(`<a id="des3" href="" style="color:#000000;border-radius: 24px;width:100%;margin:-500px;background-image: url(sico/1575471979567.jpg);" ><i class="fa fa-refresh fa-spin fa-3x fa-fw" aria-hidden="true"></i>
+<span class="sr-only">Refreshing...</span>‎ ‎ 
+  تحـديـث‎‎</a>
+<CENTER>
+<TABLE BORDER=1 WIDTH=392 HEIGHT=178 Background="sico/1573748080426.png">
+<TR><TD ALIGN=CENTER><h1>
+<font size="4" color="#000000" background="#000000" algin="left">
+<marquee direction="right" width="50%" id="des0"
+style="width:65%;margin-top:110px;margin-Right: 100px;background-image: url(sico/1575471979567.jpg);border-radius: 30px;"
+onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"><font color="#ffffff">&nbsp;اهلا وسهلا بكم في شات <font color="#ffffff" class="des2"> فهآوة للجوال </font><font color="#ffffff" class="des2">
+جولات هذا الاسبوع 🏆: يوم السبت | بطل سلة الفواكه الاسبوعي 🍓🍍 & اطرد خصمك 🔪 | . . يوم الأحد | اجمل صورة & ابواب الكنز | يوم الخميس | بطل المسابقات الثقافية + بطل روم الكنز | . . يوم الجمعة | بطل روم الجولات الأسبوعية | . . جميعها ستقام الساعة 11 مساءً</font>
+</marquee>
+ </marquee></font></b></td>
+</TD></TR></TABLE></center>`).insertBefore('.nav-tabs');
+
+
+
+
+
+$(`<style>
+.nav-tabs {margin-top: 20px!important;}
+ul.nav.nav-tabs.fl li a {      color: #756C55;  box-shadow: inset 0 2px 2px rgb(112, 112, 113), 0 0 8px rgb(112, 112, 113);font-size: 13px!important;padding: 6px!important;text-align: center!important;width: 100%;}
+ul.nav.nav-tabs.fl li {border-bottom: 3px;width: 33.3%;}
+.label.fl.loginstat {width: 35%!important;border-radius: 0px 15px 15px 0px!important;margin-top: -140px!important;}
+.label.label-primary.mini.fl {width: 50%!important;margin-left: 230px!important;margin-top: -170px!important;border-radius: 15px 0px 0px 15px!important;}
+a.label.label-primary.fl {
+  display:none;
+
+}
+input#u2,input#u3,input#u1 {width: 70%!important;margin-left: -2px!important;border-radius: 15px!important;}
+input#pass1,input#pass2 {width: 70%!important;border-radius: 15px!important;}
+
+</style>`).insertBefore('body');
+$('.label.label-primary.mini.fl').css({"background-image":"url(sico/1575471979567.jpg)"});
+$('.label.label-primary.mini.fl').css('color','#ffffff');
+$(`<style>
+
+ul.nav.nav-tabs.fl li a {
+  background:linear-gradient(163deg,#911969 5%,#ffffff 70%) !important;color: #434C4D;
+    margin: ;
+    text-align: center;
+    width: 150px;
+  height: 200px
+    border-radius: 50px;
+    height: 7% !important;
+    margin-top: 0px;
+    padding: 0px 0 !important;
+    font-family: 'jazeera',FontAwesome;
+    margin-right: 0px;
+    border: 0px solid !important;
+    float: right;
+}
+
+
+div#l1.grow.break.tab-pane.fade.active.in {
+    width: 90% !important;
+    
+    
+    height: 95px;
+    background-color: #ffffff;
+    background-image: -webkit-linear-gradient(45deg,rgba(172, 38, 114,.2) 25%,transparent 25%,transparent 50%,rgba(172, 38, 114,.2) 50%,rgba(172, 38, 114,.2) 75%,transparent 75%,transparent) !important;
+    border-radius: 30px;
+    margin-left: 20px
+}
+
+div#l2,
+div#l3 {
+  width: 90% !important;
+  
+  height: 95px;
+  border-radius:30px 30px 30px 0px;
+  background-color: #ffffff;
+  background-image:-webkit-linear-gradient(45deg,rgba(172, 38, 114,.2) 25%,transparent 25%,transparent 50%,rgba(172, 38, 114,.2) 50%,rgba(172, 38, 114,.2) 75%,transparent 75%,transparent) !important;
+   margin-left: 20px;
+}
+img {onload: NcodeImageResizer.createOn(this);border: 5;originalWidth: 250;originalHeight: 270;}
+</style>`).insertBefore('body');
+$.getScript("https://cdn.statically.io/gh/a7mdoof/fhh0/0d229752/Fhh_alwan_btn.js");
