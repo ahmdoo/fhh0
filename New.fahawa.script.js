5da5797dcdd638743c02369cb2ff6d9583b12228
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2021-11-26 06:39:17 +0300

    Create New.fahawa.script.js

---
+++ New.fahawa.script.js
@@ -0,0 +1,115 @@
+$(`<style>a.label.label-primary.fl
+	{
+	display:none
+}
+button.btn.fr.btn-success.fa.fa-refresh
+	{
+	display:none
+}
+a.label.label-primary.fl img.fl
+
+a.label.label-primary.fl
+	{
+	border-radius:3px!important;
+	margin:2px-0px;
+	padding:5px!important;
+	margin-bottom:2px;
+	text-align:right!important
+}
+#des22
+	{
+	text-align:center!important;
+	font-size:100%!important
+}
+label.fl.label.loginstat
+	{
+
+	position:absolute;
+	top:0;
+	margin-top:4px;
+	Right:0
+}
+.fr.borderg,.fr.borderg a
+	{
+	font-size:10px!important;
+	padding:0 5px!important
+}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success
+	{
+	padding:0px 3px 2px;
+	margin-right:5px
+}
+
+	
+.fr.borderg,.fr.borderg a
+	{
+	border-radius:3px!important;
+	margin-right:0px!important;
+	border:black solid 0px;
+	color:#7D7D7D
+}
+ul.nav.nav-tabs>li>a
+	{
+	color:#7D7D7D;
+	border:none;
+	text-align:center;
+	border-radius:3px;
+	display:initial;
+	width:80%;
+	float:left;
+	padding:0!important
+}
+ul.nav.nav-tabs>li.active>a
+	{
+	border:none;
+	border-radius:3px
+}
+ul.nav.nav-tabs>li
+	{
+	width:32%;
+	margin:0 5px 0 0
+}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user:before
+	{
+	float:right;
+	margin-right:-26%;
+	border:1px solid;
+	padding:0 2px 1px;
+	border-radius:3px;
+	width:23px;
+	text-align:center;
+	color:#7D7D7D
+}
+ul.nav.nav-tabs
+	{
+	border-top:1px solid#190604;
+	margin-top:1px;
+	float:right;
+	width:100%;
+	padding:3px 1px
+}
+input#u1,input#u2,input#pass1,input#u3,input#pass2
+	{
+	width:50%;
+	height:31px;
+	margin-left:40.5px;
+	margin-bottom:5.5px;
+	border:1px solid#D1351D;
+	border-radius:3px;
+	text-align:center
+}
+div#asim13
+	{
+	width:49%;
+	top:122px;
+	position:absolute;
+	left:3px;
+
+}
+div#l1
+	{
+	padding:16.8px!important
+}
+
+</style>`).insertBefore("body");
+
