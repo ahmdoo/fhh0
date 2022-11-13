12258c534c785bba4011136b6a3d309196722acc
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2022-11-14 00:32:57 +0300

    Update New.style.js

--- New.style.js
+++ New.style.js
@@ -5,12 +5,12 @@ $(`<style>.nosel.d-flex.room.r184721046d6xd95d5701-xcyewdv.bord {border-inline: 
 
 background-size: 100%; 
 border-radius: 0px 0px 25px 25px;
-box-shadow: 0px 0px 4px 4px #00eaff38;
+box-shadow: 0px 0px 4px 4px #006600;
 }
 
 .nosel.d-flex.room.r184721046d6xd95d5701-xcyewdv .dots.mini.u-msg {text-align: center;background-size: 200% auto; -webkit-background-clip: text;-webkit-text-fill-color: #fff;background-image: url(https://j.top4top.io/p_2508ne23l0.gif);}
 
-.nosel.d-flex.room.r1184721046d6xd95d5701-xcyewdv .fitimg.u-pic.borderg{border-radius: 100px;border: 4px solid #1D7823;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;}
+.nosel.d-flex.room.r1184721046d6xd95d5701-xcyewdv .fitimg.u-pic.borderg{border-radius: 100px!important;border: 4px solid #1D7823;letter-spacing: 1px;background-size: 108% 108%;-webkit-text-fill-color: white;animation: grad 2s cubic-bezier(.445,.05,.55,.95) alternate infinite;}
 
 
 
@@ -20,18 +20,19 @@ box-shadow: 0px 0px 4px 4px #00eaff38;
 
 
  
-.nosel.d-flex.room.r184721046d6xd95d5701-xcyewdv .corner.fa.fa-user.label.uc.fa-microphone.label-danger {background-color: transparent;letter-spacing: 1px;-webkit-text-fill-color: #000;padding: 0px !important;border-radius: 50px !important;background-color: #transparent!important;}
+.nosel.d-flex.room.r184721046d6xd95d5701-xcyewdv .corner.fa.fa-user.label.uc.fa-microphone.label-danger {background-color: transparent;letter-spacing: 1px;-webkit-text-fill-color: #fff;padding: 0px !important;border-radius: 50px !important;background-color: #transparent!important;}
 
-.nosel.d-flex.room.r184721046d6xd95d5701-xcyewdv .u-topic.dots {text-transform: uppercase;background-image: linear-gradient(-225deg, #000 0%, #2F7838 29%, #2F7838 67%, #000 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 2s linear infinite;display: inline-block;}
+.nosel.d-flex.room.r184721046d6xd95d5701-xcyewdv .u-topic.dots {text-transform: uppercase;background-image: linear-gradient(-225deg, #fff 0%, #2F7838 29%, #2F7838 67%, #fff 100%);background-size: 220% auto;-webkit-background-clip: text;-webkit-text-fill-color: transparent;animation: textclip 2s linear infinite;display: inline-block;}
 @keyframes textclip {to {background-position: 200% center;}}</style>`).insertBefore('body');
 var rid='184721046d6xd95d5701-xcyewdv';
 
- $(`<style>#mic.aym3 .mic {
+
+$(`<style>#mic.aym3 .mic {
         border: 2px solid;
-        border-image: linear-gradient(var(--angle), #2C692C 0%, #000 29%, #2F7838 67%, #020202 100%) 1;
+        border-image: linear-gradient(var(--angle), #2C692C 0%, #fff 29%, #2C692C 67%, #fff 100%) 1;
         animation: 5s rotate linear infinite;
         --angle: 0deg;
-        background-color: #e3e3e3;
+        background-color: #fff;
         border-radius: 5px !important;
         margin: 2px;margin-top: 1px;box-shadow: inset 0 0px 0px rgb(0 0 0 / 8%), 0 0px 1px #ffffff;
         height: 48px;}
@@ -45,17 +46,14 @@ var rid='184721046d6xd95d5701-xcyewdv';
         syntax: "<angle>";
         initial-value: 0deg;
         inherits: false;
-
     }</style>`).insertBefore('body');
 
 
-
-
-$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{border: 0px solid #fff;background-image: linear-gradient(-225deg, #fff 0%, #2C692C 29%, #2C692C 67%, #000 100%);
+$("style:eq(0)").append(`#d2 .roomh[onclick="rjoin('${rid}')"]{border: 0px solid #fff;background-image: linear-gradient(-225deg, #fff 0%, #2C692C 29%, #2C692C 67%, #fff 100%);
 
 background-size: 220% auto;animation: textclip 5s linear infinite;border-radius: 50px !important;background-color: #fff !important;
 }`);
-$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{border: 0px solid #0000;background-image: linear-gradient(-225deg, #fff 0%, #2C692C 29%, #2C692C 67%, #000 100%);
+$("style:eq(0)").append(`#upro .roomh[onclick="rjoin('${rid}')"]{border: 0px solid #0000;background-image: linear-gradient(-225deg, #fff 0%, #2C692C 29%, #2C692C 67%, #fff 100%);
 background-size: 220% auto;animation: textclip 5s linear infinite;border-radius: 50px !important;background-color: #fff !important;
 }`);
 
@@ -66,13 +64,13 @@ background-size: 220% auto;animation: textclip 5s linear infinite;border-radius:
     
     #users.aym1 .inroom.uzr img.ustat {
       width: 54px !important;
-      border-radius: 50px 50px 50px 50px;
+      border-radius: 50px 50px 50px 50px!important;
       height: 52px;
       margin-top: -1px;
       min-height: 0% !important;
     }
 #users.aym1 .inroom.uzr .fitimg.u-pic {
-      border-radius: 50px 50px 50px 50px;
+      border-radius: 50px 50px 50px 50px!important;
     margin-top: 0px;
     z-index: 0;
     margin-left: -51px;
@@ -85,7 +83,7 @@ background-size: 220% auto;animation: textclip 5s linear infinite;border-radius:
     #d2.aym2 .uzr .fitimg.u-pic{
     border: 2px solid #2C692C;
 background-color: #2C692C;
- border-radius: 50px 50px 50px 50px;
+ border-radius: 50px 50px 50px 50px!important;
         background-color: #2C692C;
 }
 #d2.aym2 .uzr .d-flex.mm{
@@ -95,9 +93,9 @@ background-color: #2C692C;}
     #users.aym1 .uzr .u-msg{
 	font-weight:bold;
         text-align: center;
-        -webkit-text-fill-color: #000;
+        -webkit-text-fill-color: #fff;
         text-transform:uppercase;
-        background-image: linear-gradient(327deg, #2C692C, #000000);
+        background-image: linear-gradient(327deg, #2C692C, #fff);
     background-size: 100%;
     -webkit-background-clip:text;
     -webkit-text-fill-color:transparent;animation:1s linear infinite textclip;
@@ -105,14 +103,14 @@ background-color: #2C692C;}
 
     #users.aym1 .inroom.uzr span.uhash{
     text-transform:uppercase;
-    background-image: linear-gradient(327deg, #2C692C, #000000);
+    background-image: linear-gradient(327deg, #2C692C, #fff);
     background-size:220% auto;
     -webkit-background-clip:text;
     -webkit-text-fill-color:transparent;animation:2s linear infinite textclip;
     display:inline-block;}
     
     #users.aym1 .uzr {
-        background-color: #0000 !important;}
+        background-color: #fff !important;}
       #users.aym1 {
         background-image: url("https://k.top4top.io/p_250820wm50.jpg");
   background-size: cover;
@@ -134,9 +132,9 @@ background-color: #2C692C;}
   }
 
     #d0.aym {background-color: #f6f6f6;}
-    #d0.aym .label-primary {background-color: #007aad;}
+    #d0.aym .label-primary {background-color: #2C692C;}
     chats
-    #settings.aym4 .label-primary {background-color: #000 !important;}
+    #settings.aym4 .label-primary {background-color: #fff !important;}
 
     #rooms.aym4 .label-primary {background-color: #2C692C !important;}
 	
@@ -149,7 +147,7 @@ background-color: #2C692C;}
         background-color: #f5f5f5 !important;} 
 
 		#d2.aym2 .uzr.d-flex.mm.hmsg {
-        background-color: #99ebff2b!important;
+        background-color: #2C692C!important;
 }
 
 #d2bc.aym6.d2.light.d2bc.flex-grow-1.break {
@@ -158,7 +156,7 @@ background-color: #2C692C;}
   background-color: #f5f5f5 !important;} 
 
 #users .uzr.stl1 .u-msg{
-background-color:#000;
+background-color:#fff;
 text-color:#fff;}
 
 d2.aym2.flex-grow-1.break.light{
@@ -167,30 +165,30 @@ d2.aym2.flex-grow-1.break.light{
   background-color: #f5f5f5 !important;}
 
 #dpnl.aym7.nosel.ninr.fl.uzr.label.bg {
-    background-color: #000;
--webkit-text-fill-color: #000;
+    background-color: #fff;
+-webkit-text-fill-color: #fff;
   }
 
 
 #dpnl.aym7.label.nosel.ninr.fl.uzr.label.bg {
-	-webkit-text-fill-color: #000;
+	-webkit-text-fill-color: #fff;
 
     background-color: #2C692C;
-    color: black;
+    color: #fff;
   }
 #dpnl.aym7 .nosel.ninr.fl.uzr.label.bg {
--webkit-text-fill-color: #000;
+-webkit-text-fill-color: #fff;
 
     background-color: #2C692C;
   }
 #users.aym1.nosel.ninr.fl.uzr.label.bg {
--webkit-text-fill-color: #000;
+-webkit-text-fill-color: #fff;
 
     background-color: #2C692C;
   }
 
 #users.aym1.label.nosel.ninr.fl.uzr.label.bg {
--webkit-text-fill-color: #000;
+-webkit-text-fill-color: #fff;
 
     background-color: #2C692C;
   }
