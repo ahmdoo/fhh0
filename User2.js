5a39f0f6a85ac87c91673de64e67a5f1b868e99a
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2023-02-05 02:25:52 +0300

    Update and rename User2.js to User2.js

--- User2.js
+++ User2.js
@@ -104,7 +104,7 @@ border-radius: 19px;
 var mua2=setInterval(function(){
     if(myid!=null){
         clearInterval(mua2);
-       $("#users .uzr:contains('@:')").addClass('muh');
+       $("#users .uzr:contains('June')").addClass('muh');
 		
 		
 		
