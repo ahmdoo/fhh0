813e8c7b1076ac0341c5f469c5c15b35f1c38198
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2023-02-05 01:47:04 +0300

    Create June.js

---
+++ June.js
@@ -0,0 +1,118 @@
+$("style").last().append(`
+
+#users .uzr.muh img.co {
+  transform: rotate(90deg);
+  margin-top: -60px !important;
+  margin-right: 340px;
+}
+
+#users .uzr.muh span.uhash{
+    margin-right: 350px;
+  margin-top: 24px;
+  margin-left: 0px;}
+
+
+#users .uzr.muh img.u-ico {
+    
+     
+
+      
+    }
+
+#users .uzr.muh img.ustat {
+      width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 14px;
+  z-index: 0 !important;
+  display: none;
+      
+    }
+
+#users .uzr.muh .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 23px !important;
+  height: 52px !important;
+  margin-left: 25px!important;
+  z-index: 0 !important;
+margin-bottom: 9px !important;
+padding: 30px !important;
+}
+
+#users .uzr.muh .u-msg {
+	text-align: center;
+  -webkit-background-clip: text;
+  
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2023/01/167502776201271.gif);
+  padding: 0px !important;
+	margin-bottom: 25px !important;
+margin-left: 18px;
+margin-right: 50px;
+background-size: cover;
+margin-top: 17px !important;
+
+}
+
+
+
+
+
+#users .uzr.muh {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 19px;
+   
+  
+  border: 1px solid #fff;
+ background-image: url(https://k.top4top.io/p_2591uh1ft0.png);
+background-size: contain;
+background-repeat: no-repeat;
+background-position: right; 
+}
+
+
+
+#users .uzr.muh .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.muh .u-topic {
+-webkit-text-fill-color: transparent;
+  margin-left: 5px;
+}
+
+#users .uzr.muh .itarr_muh {
+ width: 132px;
+height: 89px;
+margin-top: -53px;
+cursor: pointer;
+margin-left: -65px;
+border-radius: 19px;
+}
+
+
+
+
+
+`);
+
+var mua2=setInterval(function(){
+    if(myid!=null){
+        clearInterval(mua2);
+       $("#users .uzr:contains('June')").addClass('muh');
+		
+		
+		
+		
+        $("#users .uzr.muh .fitimg.u-pic").append('<img class="itarr_muh" title="اطاري" src="https://l.top4top.io/p_25910b4jj0.png">');
+		
+		
+		
+
+    }
+}, 20000);
