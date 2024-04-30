2b4361f393b803088e3c1f7eb8b41acecc0ce872
ahmdoo <51219714+ahmdoo@users.noreply.github.com>
2024-05-01 02:55:18 +0300

    Create Muh.js

---
+++ Muh.js
@@ -0,0 +1,104 @@
+$(`<style>
+div#l1 input#u1,div#l3 button{width:99%;margin-left:1px}
+div#l2 button,div#l3 button{border:1px solid #ccc!important;border-radius:24px!important;height:27px;padding:0 5px}
+div#l1 input#u1{border-radius:15px;border:1px solid #e1e0e0;margin-top:3px;text-align:center}
+div#l2 input#pass1,div#l2 input#u2,div#l3 input#pass2,div#l3 input#u3{width:49%;padding:0!important;float:right;margin:2px 5px 2px 0}
+div#l2 input#u2,div#l3 input#u3{margin-left:-6px;border-radius:15px;border:1px solid #e1e0e0;text-align:center}
+div#l2 input#pass1,div#l3 input#pass2{border-radius:15px;border:1px solid #e1e0e0;text-align:center}
+div#l2,div#l3{padding:2px!important}
+div#l1{margin:0 1px -18px;padding:0!important}
+div#l1,div#l2,div#l3{border-bottom:2px solid;margin-bottom:2px;border-top:2px solid;margin-top:0;border-color:#bababa;border-radius:15px ;taas:69px}
+div#l1 button{border:1px solid #ccc!important;border-radius:24px!important;width:99%;height:27px;padding:0 5px}
+div#l2 button{width:70%}
+
+ul.nav.nav-tabs>li>a{border:none;text-align:center;border-radius:3px;display:initial;width:80%;float:left;padding:0!important}
+
+ul.nav.nav-tabs>li.active>a{border:none;border-radius:3px}ul.nav.nav-tabs>li{width:32%;margin:0 5px 0 0}
+ul.nav.nav-tabs>li>.fa-user-plus:before,ul.nav.nav-tabs>li>.fa-user:before,ul.nav.nav-tabs>li>.fa-user :before{float:right;margin-right:-26%;padding:0 2px 1px;border-radius:3px;width:23px;text-align:center}
+.nav>li>a:focus,.nav>li>a:hover{color:#9abff9}
+ul.nav.nav-tabs{margin-top:2px;float:right;width:100%;padding:3px 1px}
+
+div#tlogins::-webkit-scrollbar{width:0px;height:10px}
+
+div#tlogins .fl.mini.u-msg{margin-top:3px!important;border-radius:3px!important}
+a.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh,div#l2 br,div#l3 br{display:none}
+
+
+a.label.label-primary.fl {
+    hangganan-radius: 3px!mahalaga;
+    margin: 2px -155px;
+    padding: 5px!mahalaga;
+    margin-ibaba: 2px;
+    text-align: tama! mahalaga;
+}
+
+button.btn.fr.btn-success.fa.fa-refresh{display:none}
+a.label.label-primary.fl img.fl{display:none}
+#des22{text-align:center!important;font-size:100%!important}
+.fr.borderg,.fr.borderg a{font-size:10px!important;padding:0 5px!important;position:relative;color:#ffffff}
+.fr.borderg{border:0;margin-top:0;height:0}
+span.s1.fa.fa-user.label.badgex.label-as-badge.label-success{padding:0 3px 2px;margin-right:5px}
+label.label.label-primary.mini.fl{padding:1px;border-radius:3px!important}
+
+
+
+
+.btn.fr.btn-success.fa.fa-refresh {
+  display: wala!mahalaga;
+}
+
+span.flex-grow-1.dots {
+  text-align: center!important;
+}
+
+.label.bg.d-flex.fl {
+  border-radius: 3px !important;
+  margin: 1px 2px;
+    margin-ibaba: 1px;
+  margin-ibaba: 1px;
+  padding: 0px !important;
+  margin-ibaba: 2px;
+  text-align: tama !importante;
+}
+
+div#l1 input#u1 {
+  border-radius: 15px;
+  hangganan: 1px solid #e1e0e0;
+  margin-top: 3px !important;
+  text-align: center;
+  taas: 25px !importante;
+  lapad: 95% !importante;
+  margin-kaliwa: 10px;
+}
+
+#tlogins {
+  max-width: 394px !important;
+}
+div#l1 na buton {
+  hangganan: 1px solid #ccc !important;
+  hangganan-radius: 24px !mahalaga;
+  lapad: 95%!mahalaga;
+  taas: 27px!mahalaga;
+  padding: 0 5px!importante;
+  margin-left: 10px!important;
+}
+span.btn.fr.fa.fa-eye {
+  transform: matrix(1,0,0,1,-40,-3)!mahalaga;
+}
+
+.fa.fa-user.label.badex.label-success {
+  transform: matrix(1,0,0,1,-55,0);
+}
+
+div#l2 input#pass1, div#l2 input#u2, div#l3 input#pass2, div#l3 input#u3 {
+  lapad: 48% !mahalaga;
+  padding: 0 !mahalaga;
+  lumutang: kanan;
+  margin: 2px 5px 2px 0;
+    margin-left: 0px;
+  taas: 25px !importante;
+}
+
+
+
+</style>`).insertBefore('body');
